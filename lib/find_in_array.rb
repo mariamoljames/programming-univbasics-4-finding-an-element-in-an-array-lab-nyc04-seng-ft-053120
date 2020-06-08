88bdaaa7fd8b65388d34ee1b05648b5189341a95
Mariamol James <mariamol.james@gmail.com>
2020-06-08 20:28:26 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
   count=0
   while count<array.length do
     if array[count]==value_to_find
-      value_index=array[count]
+      value_index=count
     end
     count+=1
   end
